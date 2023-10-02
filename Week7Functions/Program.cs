483eeeda851a530c6fb50852b491aea96564c1ed
TaagoT <taago.tagam@tktk.ee>
2023-10-02 16:00:51 +0300

    add Week7Functions code

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoice = Console.ReadLine();
+
+if (userChoice == "in" )
+{
+    PrintHello();
+}
+else
+{
+    printGoodbye();
+}
+
+
+
+
+
+static void PrintHello () //function ehk või method
+{
+Console.WriteLine("Hello, world!");
+}
+
+static void printGoodbye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
+
